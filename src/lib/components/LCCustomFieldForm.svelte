cbb8764ba16e8123ccbb32efaeab7cd0bd4065b4
Kaustubh Bhalerao <bhalerao@soildiagnostics.com>
2023-08-04 16:57:40 -0500

    changes to pkg json

--- src/lib/components/LCCustomFieldForm.svelte
+++ src/lib/components/LCCustomFieldForm.svelte
@@ -2,7 +2,6 @@
 	import LCCustomFieldFormField from './LCCustomFieldFormField.svelte';
 	export let formData;
 	$: groupClass = 'lc-cf-formfield';
-	$: groupId = 'cf-form';
 </script>
 
 {#each formData as formField (formField.id)}
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,9 @@
 // Reexport your entry components here
+import LCCustomFieldForm from "./components/LCCustomFieldForm.svelte";
+import LCCustomFieldFormField from "./components/LCCustomFieldFormField.svelte";
+
+const LCCustomFieldComponents = {
+    LCCustomFieldForm, LCCustomFieldFormField
+}
+
+export default LCCustomFieldComponents;
